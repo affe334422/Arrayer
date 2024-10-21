01b181482562f95fe719b55ba16f017ceacdceea
affe334422 <alle07002@utb.vaxjo.se>
2024-10-21 10:56:03 +0200

    ll

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Headers;
+Mai();
+Console.WriteLine("bög");
+static void Mai(){
+    double andtal = 0;
+	double[] temperatur={4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9}; //7
+    for(int a = 0; a != 7; a++){
+        andtal = temperatur[a] + andtal;
+    }
+    andtal = andtal / 7;
+    Console.WriteLine("medel värdet denna veckan är "+ andtal);
+	//Lägg in programkod här för att beräkna medelvärdet…
+}
